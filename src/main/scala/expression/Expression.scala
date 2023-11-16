289972f474c9e33a79e1889380e772b73e84e4c3
Martin Vidmar <martin.vidmar@marketup.cz>
2023-11-16 14:38:38 +0100

    wip: remove FinalExpression

--- src/main/scala/expression/Expression.scala
+++ src/main/scala/expression/Expression.scala
@@ -8,10 +8,11 @@ abstract class ExpressionEvaluationContext {
     def get(pos: TableCellPosition): Expression
 }
 
-abstract class ConstantExpression extends Expression with FinalExpression {
-    override def evaluate(context: ExpressionEvaluationContext): FinalExpression = this
+abstract class ConstantExpression extends Expression {
+    override def evaluate(context: ExpressionEvaluationContext): ConstantExpression = this
     def getOne: Option[ConstantExpression] = Some(this)
     override def isAtomic: Boolean = true
+    override def referencedPositions: Iterable[TableCellPosition] = Iterable.empty
 }
 
 final case object EmptyExpression extends ConstantExpression
@@ -19,18 +20,8 @@ final case class IntExpression(value: Int) extends ConstantExpression
 final case class FloatExpression(value: Double) extends ConstantExpression
 final case class StringExpression(value: String) extends ConstantExpression
 
-final case class ArrayExpression(
-    expressions: Array[Expression]
-) extends Expression with IterableExpression[Expression] with FinalExpression {
-    def referencedPositions: Iterable[TableCellPosition] = {
-        expressions.flatMap(expr => expr.referencedPositions)
-    }
-    def elements(context: ExpressionEvaluationContext): Iterable[Expression] = expressions
-    def getOne: Option[ConstantExpression] = None
-}
-
 abstract class Expression {
-    def evaluate(context: ExpressionEvaluationContext): FinalExpression
+    def evaluate(context: ExpressionEvaluationContext): ConstantExpression
     def referencedPositions: Iterable[TableCellPosition]
     def isConstant: Boolean = referencedPositions.take(1).size == 0
     def isAtomic: Boolean = false
@@ -38,9 +29,4 @@ abstract class Expression {
 
 trait IterableExpression[T <: Expression] extends Expression {
     def elements(context: ExpressionEvaluationContext): Iterable[T]
-}
-
-trait FinalExpression extends Expression {
-    override def referencedPositions: Iterable[TableCellPosition] = Iterable.empty
-    def getOne: Option[ConstantExpression]
 }
