2c56d204ba4ffb0ece0fe29fe72f1b3a27626c2f
Martin Vidmar <martin.vidmar@marketup.cz>
2023-11-16 14:40:24 +0100

    wip: remove RangeExpression

--- src/main/scala/expression/ReferenceExpression.scala
+++ src/main/scala/expression/ReferenceExpression.scala
@@ -3,8 +3,8 @@ package expression
 import table.{TableCellPosition, TableCellValue}
 
 final case class ReferenceExpression(pos: TableCellPosition) extends Expression {
-    def evaluate(context: ExpressionEvaluationContext): Expression = {
-        context.get(pos)
+    def evaluate(context: ExpressionEvaluationContext): ConstantExpression = {
+        context.get(pos).evaluate(context)
     }
     def referencedPositions: Iterable[TableCellPosition] = Array(pos)
 }
